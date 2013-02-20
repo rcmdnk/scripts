0f23eb6a5420e7ac87efb7db6c5e52c0d460c32b
rcmdnk <Michiru.Kaneda@gmail.com>
2013-02-20 20:24:15 +0100

    update from linux-gnu

--- cleanTrash.sh
+++ cleanTrash.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-MAXTRASHSIZE=1000
-TRASH_BOX_DIR="$HOME/.trash/"
+. ~/.bashrc
+if [ "$TRASH" = "" ];then
+  echo "please set TRASH"
+  exit
+fi
 FLAG="FALSE"
 while [ $FLAG = "FALSE" ];do
-  TRASH_BOX_SIZE=`du -ms ${TRASH_BOX_DIR} |awk '{print $1}'`
+  TRASH_BOX_SIZE=`du -ms ${TRASH} |awk '{print $1}'`
   if [ ${TRASH_BOX_SIZE} -gt ${MAXTRASHSIZE} ];then
     DELETE_DIR=`ls ${TRASH_BOX_DIR} | head -1`
-    rm -rf ${TRASH_BOX_DIR}/${DELETE_DIR}
+    rm -rf ${TRASH}/${DELETE_DIR}
   else
     FLAG="TRUE"
   fi
--- trash.sh
+++ trash.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
+. ~/.bashrc
+if [ "$TRASH" = "" ];then
+  echo "please set TRASH"
+  exit
+fi
 if [ "$#" -lt 1 ]
 then
   echo "enter junk files or directories"
 else
-  TRASH_BOX="$HOME/.Trash/`date +%Y%m%d`"
-  if [ ! -d $TRASH_BOX ];
+  if [ ! -d $TRASH ];
   then
-    mkdir -p $TRASH_BOX
+    mkdir -p $TRASH
   fi
 
   while [ "$#" -gt 0 ];do
     NAME=`echo $1 | sed -e "s|/$||" | sed -e "s|.*/||"`
-    TRASH_HEAD=${TRASH_BOX}/${NAME}
+    TRASH_HEAD=${TRASH}/${NAME}
     TRASH_NAME=${TRASH_HEAD}
     i=1
     while true;do
