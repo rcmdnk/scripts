4dbb10c83d1eb04b418c483908cce48bf3ac26f7
rcmdnk <rcmdnk@gmail.com>
2013-02-21 00:18:53 +0100

    update from darwin11

--- install.sh
+++ install.sh
@@ -1,25 +1,28 @@
 #!/bin/sh
 backup="bak"
 exclude=('.' '..' 'README.md' 'install.sh')
+overwrite=1
+notinstalled=()
 instdir=$HOME/usr/bin
 curdir=`pwd -P`
 # help
-
-HELP="Usage: $0 [-b <backup file postfix>] [-e <exclude file>] [-i <install dir>]
+HELP="Usage: $0 [-n] [-b <backup file postfix>] [-e <exclude file>] [-i <install dir>]
 
 Arguments:
       -b  Set backup postfix (default: make *.bak file)
           Set \"\" if backups are not necessary
       -e  Set additional exclude file (default: ${exclude[@]})
       -i  Set install directory (default: $instdir)
+      -n  Don't overwrite if file is already exist
       -h  Print Help (this message) and exit
 "
-while getopts b:e:i:h OPT;do
+while getopts b:e:i:nh OPT;do
   OPTNUM=`expr $OPTNUM + 1`
   case $OPT in
     "b" ) backup=$OPTARG ;;
     "e" ) exclude=(${exclude[@]} $OPTARG) ;;
     "i" ) instdir=$OPTARG ;;
+    "n" ) overwrite=0 ;;
     "h" ) echo "$HELP" 1>&2; exit ;;
     * ) echo "$HELP" 1>&2; exit ;;
   esac
@@ -39,8 +42,13 @@ for f in *.sh;do
   fi
   name=${f%.sh}
   echo "install $f to $instdir, as $name"
+  install=1
   if [ "`ls $instdir/$name 2>/dev/null`" != "" ];then
-    if [ "$backup" != "" ];then
+    if [ $overwrite -eq 0 ];then
+      echo "$f exists, don't install"
+      notinstalled=(${notinstalled[@]} $f)
+      install=0
+    elif [ "$backup" != "" ];then
       echo "$name exists, make backup ${name}.$backup"
       mv $instdir/$name $instdir/${name}.$backup
     else
@@ -48,5 +56,12 @@ for f in *.sh;do
       rm $instdir/$name
     fi
   fi
-  ln -s $curdir/$f $instdir/$name
+  if [ $install -eq 1 ];then
+    ln -s $curdir/$f $instdir/$name
+  fi
 done
+if [ $overwrite -eq 0 ];then
+  if [ ${#notinstalled[@]} != 0 ];then
+    echo "following files were not installed: ${notinstalled[@]}"
+  fi
+fi
