ff1bbe3742f6e4652f1ea64d0e69efb6bee554ae
rcmdnk <rcmdnk@gmail.com>
2013-02-20 20:17:03 +0100

    update from mac

--- 256col.sh
+++ 256col.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 for c in {0..255};do
-  echo -n $'\e[38;5;'${c}"m"`printf " %03d" ${c}`$'\e['"m"
-  echo -n $'\e[48;5;'${c}"m"`printf " %03d" ${c}`$'\e['"m"
+  num=`printf " %03d" $c`
+  printf "\e[38;5;${c}m$num\e[m"
+  printf "\e[48;5;${c}m$num\e[m"
   if [ $(($c%8)) -eq 7 ];then
     echo
   fi
