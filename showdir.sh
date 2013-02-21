b4be636e9af6191064150216af0d46126b4bf84d
rcmdnk <rcmdnk@gmail.com>
2013-02-21 15:08:06 +0100

    update from linux-gnu

--- showdir.sh
+++ showdir.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-maxlen=15
+maxlen=20
 dir="${PWD/#$HOME/~}"
 if [ ${#dir} -gt $maxlen ];then
   dir=!`echo $dir | cut -b $((${#dir}-$maxlen+2))-${#dir}`
 fi
 if [[ "$TERM" =~ "screen" ]]; then
   printf "\ek$dir\e\\"
-  printf "\eP\e]0;%s@%s:%s\a\e\\" "${USER}" "${HOSTNAME%%.*}" "${PWD/#$HOME/~}"
+  #printf "\eP\e]0;%s@%s:%s\a\e\\" "${USER}" "${HOSTNAME%%.*}" "${PWD/#$HOME/~}"
 else
   printf "$dir\n"
 fi
