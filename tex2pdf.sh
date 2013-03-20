43eb55135bbceec558ad3b42fe64d8ce3472cb13
rcmdnk <Michiru.Kaneda@gmail.com>
2013-03-20 17:43:05 +0100

    tex2pdf.sh, from darwin11

--- tex2pdf.sh
+++ tex2pdf.sh
@@ -4,18 +4,22 @@ then
   echo "enter one tex file name"
   echo "(enter \"name\" for file \"name.tex\")"
 else
-  NAME=`echo $1|awk '{split($1,tmp,".tex$")}{print tmp[1]}'`
-  if [ -z $NAME ];then
-    NAME=$1
+  name=`echo $1|awk '{split($1,tmp,".tex$")}{print tmp[1]}'`
+  if [ -z $name ];then
+    name=$1
   fi
-  platex $NAME
-  platex $NAME
-  bibtex $NAME
-  bibtex $NAME
-  platex $NAME
-  platex $NAME
-#  pdvips $NAME -o $NAME.ps
-#  ps2pdf $NAME.ps $NAME.pdf
-  dvipdfm $NAME.dvi
-  #rm -f $NAME.dvi $NAME.log $NAME.aux $NAME.ps
+  echo $name.aux
+  rm -f $name.aux
+  rm -f $name.bbl
+  rm -f $name.blg
+  platex $name
+  platex $name
+  bibtex $name
+  bibtex $name
+  platex $name
+  platex $name
+  #pdvips $name -o $name.ps
+  #ps2pdf $name.ps $name.pdf
+  dvipdfm $name.dvi
+  #rm -f $name.dvi $name.log $name.aux $name.ps
 fi
