941ef78f24c2e499797c5b938003c9f564589df7
rcmdnk <rcmdnk@gmail.com>
2013-05-05 03:22:20 +0200

    evernote.sh, from darwin11

--- evernote.sh
+++ evernote.sh
@@ -2,46 +2,70 @@
 # evernote.sh
 # send note to ever note by email
 
-# constant values
+# Constant values
 TAGMARK="#"
 NOTEBOOKMARK="@"
 
+# Default values
+def_attach=false
+def_inputfile=""
+def_address=""
+def_addressother=""
+def_title="By email"
+def_tag=""
+def_notebook=""
+
 # set default values
-ATTACH=false
-INPUTFILE=""
-ADDRESS=xxxxxx@m.evernote.com
-ADDRESSOTHER=xxx@xxxxxx
-DEFTITLE="By email"
-TITLE="$DEFTITLE"
-TAG=""
-NOTEBOOK=""
+attach=$def_attach
+inputfile=$def_inputfile
+address=$def_address
+addressother=$def_addressother
+title=$def_title
+tag=$def_tag
+notebook=$def_notebook
 
 # check evernote setting
 if [ -f ~/.evetnote ];then
   addresstmp=`grep ADDRESS ~/.evetnote|cut -d= -f2`
   addressothertmp=`grep ADDRESSOTHER ~/.evetnote|cut -d= -f2`
   if [ "$addresstmp" != "" ];then
-    ADDRESS=$addresstmp
+    address=$addresstmp
   fi
   if [ "$addressothertmp" != "" ];then
-    ADDRESSOTHER=$addressothertmp
+    addressother=$addressothertmp
   fi
 fi
 
 # help
-HELP="Usage: $0 [-uh] [-f <input file>] [-a <email address>] [-o <other mail address>] [-t <title>] [-T <tag>] [-n <notebook>] message...
+HELP="Usage: $0 [-uh] [-f <input file>] [-a <email address>]
+                [-o <other mail address>] [-t <title>] [-T <tag>]
+                [-n <notebook>] message...
 
 Arguments:
-      -u  If an input file is sent as attachment or not (default: ${ATTACH})
-      -f  Set input file, in which the contents is sent (default: ${INPUTFILE})
-      -a  Set email adress (default: ${ADDRESS})
-      -o  Set other email adress for check (default: ${ADDRESSOTHER})
-      -t  Set title (default: ${TITLE} or inputFile if available)
-      -T  Set tag (default: ${TAG} ) + email (always added)
+      -u  Send an input file as attachment
+          (default: file's content is sent as an inline text)
+      -f  Set an input file, in which the contents is sent
+          (default: ${def_inputfile})
+      -a  Set an email adress (default: ${def_address})
+      -o  Set other email adress for check (default: ${def_addressother})
+      -t  Set a title (default: ${def_title} or inputFile if available)
+      -T  Set a tag (default: ${def_tag} ) + email (always added)
           To put multiple tags, use like -T \"#tag1 #tag2 #tag3\"
-      -n  Set notebook (default: ${NOTEBOOK} )
- message  Be sent as a message
+      -n  Set a notebook (default: ${def_notebook} )
       -h  Print Help (this message) and exit
+
+ message  Be sent as a message
+
+Setting file:
+      ~/.evetnote
+          ADDRESS=xxxx@m.evernote.com
+          ADDRESSOTHER=xxxx@YYY.ZZZ
+
+ADRESS for evernote is mandatory.
+Please specify it in .evernote or use -a.
+
+Note: 50 mails can be sent as a standard user.
+     250 mails can be sent as a premium user.
 "
 
 # check arguments
@@ -51,52 +75,62 @@ if [ $# -eq 0 ];then
 fi
 
 OPTNUM=0
-while getopts uf:a:t:T:n:h OPT;do
+while getopts uf:a:o:t:T:n:h OPT;do
   OPTNUM=`expr $OPTNUM + 1`
   case $OPT in
-    "u" ) ATTACH=true ;;
-    "f" ) INPUTFILE="$OPTARG" ;;
-    "a" ) ADDRESS="$OPTARG" ;;
-    "t" ) TITLE="$OPTARG" ;;
-    "T" ) TAG="$OPTARG" ;;
-    "n" ) NOTEBOOK="$OPTARG" ;;
+    "u" ) attach=true ;;
+    "f" ) inputfile="$OPTARG" ;;
+    "a" ) address="$OPTARG" ;;
+    "o" ) addressother="$OPTARG" ;;
+    "t" ) title="$OPTARG" ;;
+    "T" ) tag="$OPTARG" ;;
+    "n" ) notebook="$OPTARG" ;;
     "h" ) echo "$HELP" 1>&2; exit ;;
     * ) echo "$HELP" 1>&2; exit ;;
   esac
 done
 shift $(($OPTIND - 1))
 
-# set title if necessary
+# Check address
+if [ "$address" = "" ];then
+  echo "$HELP" 1>&2
+  exit
+fi
+if [ "$addressother" != "" ];then
+  address=${address},${addressother}
+fi
+
+# Set title if necessary
 message="$*"
-if [ "$TITLE" = "$DEFTITLE" ] && [ "$INPUTFILE" != "" ];then
-  TITLE=$INPUTFILE
+if [ "$title" = "$def_title" ] && [ "$inputfile" != "" ];then
+  title=$inputfile
 fi
 
-# tag check
-tagCheck=`echo "$TAG"|cut -c1`
-if [ "$TAG" != "" ] && [ "$tagCheck" != "$TAGMARK" ];then
-  TAG="$TAGMARK""$TAG"
+# Tag check
+tagCheck=`echo "$tag"|cut -c1`
+if [ "$tag" != "" ] && [ "$tagCheck" != "$TAGMARK" ];then
+  tag="$TAGMARK""$tag"
 fi
-TAG="$TAG #email"
+tag="$tag #email"
 
-# notebook check
-notebookCheck=`echo "$NOTEBOOK"|cut -c1`
-if [ "$NOTEBOOK" != "" ] && [ "$notebookCheck" != "$NOTEBOOKMARK" ];then
-  NOTEBOOK="$NOTEBOOKMARK""$NOTEBOOK"
+# Notebook check
+notebookCheck=`echo "$notebook"|cut -c1`
+if [ "$notebook" != "" ] && [ "$notebookCheck" != "$NOTEBOOKMARK" ];then
+  notebook="$NOTEBOOKMARK""$notebook"
 fi
 
-# check input file and send message
-if $ATTACH;then
-  if [ "$INPUTFILE" != "" ];then
-    (echo $message;uuencode $INPUTFILE `basename $INPUTFILE`)|mail -s "$TITLE $NOTEBOOK $TAG" $ADDRESS
+# Check input file and send message
+if $attach;then
+  if [ "$inputfile" != "" ];then
+    (echo $message;uuencode $inputfile `basename $inputfile`)|mail -s "$title $notebook $tag" $address
   else
-    (echo $message)|mail -s "$TITLE $NOTEBOOK $TAG" $ADDRESS
+    (echo $message)|mail -s "$title $notebook $tag" $address
   fi
 else
-  if [ "$INPUTFILE" != "" ];then
-    (echo $message;cat $INPUTFILE)|mail -s "$TITLE $NOTEBOOK $TAG" $ADDRESS
+  if [ "$inputfile" != "" ];then
+    (echo $message;cat $inputfile)|mail -s "$title $notebook $tag" $address
   else
-    (echo $message)|mail -s "$TITLE $NOTEBOOK $TAG" $ADDRESS
+    (echo $message)|mail -s "$title $notebook $tag" $address
   fi
 fi
 
