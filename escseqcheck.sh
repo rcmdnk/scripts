b7f9e7d1437fa21d7300cdec979cbcce5aca9acc
rcmdnk <rcmdnk@gmail.com>
2013-09-05 03:22:20 +0200

    colcheck.sh escseqcheck.sh, from darwin11

--- escseqcheck.sh
+++ escseqcheck.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+
 echo "Standalone"
 for i in {0..64};do
-  num=`printf " %4d" $i`
-  printf "\e[${i}m$num\e[m"
+  num=`printf "%02d" $i`
+  printf " \e[${i}m$num\e[m"
   if [ $(($i%10)) -eq 9 ];then
     echo
   fi
@@ -12,11 +13,23 @@ for i in {0..64};do
 done
 echo
 echo
+echo "Default Colors (0)"
+i=30
+while [ $i -lt 50 ];do
+  num=`printf "%02d;%2d" "0" "$i"`
+  printf " \e[1;${i}m$num\e[m"
+  if [ $(($i%10)) -eq 9 ];then
+    echo
+  fi
+  i=$((i+1))
+done
+echo
+echo
 echo "Colors + Bold or Increased Intensity (1)"
 i=30
 while [ $i -lt 50 ];do
-  num=`printf " %4s" "1;$i"`
-  printf "\e[1;${i}m$num\e[m"
+  num=`printf "%02d;%2d" "1" "$i"`
+  printf " \e[1;${i}m$num\e[m"
   if [ $(($i%10)) -eq 9 ];then
     echo
   fi
@@ -27,8 +40,8 @@ echo
 echo "Colors + Fait, Decreased Intensity or Second Color"
 i=30
 while [ $i -lt 50 ];do
-  num=`printf " %4s" "2;$i"`
-  printf "\e[2;${i}m$num\e[m"
+  num=`printf "%02d;%02d" "2" "$i"`
+  printf " \e[2;${i}m$num\e[m"
   if [ $(($i%10)) -eq 9 ];then
     echo
   fi
@@ -38,8 +51,8 @@ echo
 echo
 echo "All with Red (31)"
 for i in {0..64};do
-  num=`printf " %5s" "31;$i"`
-  printf "\e[31;${i}m$num\e[m"
+  num=`printf "%02d;%02d" "31" "$i"`
+  printf " \e[31;${i}m$num\e[m"
   if [ $(($i%10)) -eq 9 ];then
     echo
   fi
