684187af7f699a7b05e51396dbd23963971c7145
rcmdnk <rcmdnk@gmail.com>
2013-10-27 03:22:21 +0100

    gtaskslist.py, from darwin13

--- gtaskslist.py
+++ gtaskslist.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 
 import os
 import sys
@@ -16,14 +16,14 @@ def main(uselist='',items=''):
 
     if credentials is None or credentials.invalid:
         credentials = run(
-          OAuth2WebServerFlow(
-            client_id='326384869607.apps.googleusercontent.com',
-            client_secret='hm4nOe7yiMvpip0qE5s7D-AS',
-            scope=[
-              'https://www.googleapis.com/auth/tasks',
-              'https://www.googleapis.com/auth/tasks.readonly'],
-            user_agent='gtaskslist/1.0',),
-          storage)
+            OAuth2WebServerFlow(
+                client_id='326384869607.apps.googleusercontent.com',
+                client_secret='hm4nOe7yiMvpip0qE5s7D-AS',
+                scope=[
+                    'https://www.googleapis.com/auth/tasks',
+                    'https://www.googleapis.com/auth/tasks.readonly'],
+                user_agent='gtaskslist/1.0',),
+            storage)
 
     http = httplib2.Http()
     http = credentials.authorize(http)
@@ -40,20 +40,20 @@ def main(uselist='',items=''):
         # Get tasks
         tasks = service.tasks().list(tasklist=tl['id']).execute()
         for t in tasks['items']:
+
             ttitle = t['title']
             if ttitle == '':
                 continue
-            useflag=0
+            useflag = 0
             if len(items) == 0 or items[0] == '':
                 useflag = 1
             else:
                 for i in items:
-                    if ttitle.find(i):
+                    if ttitle.find(i) != -1:
                         useflag = 1
                         break
             if useflag == 1:
                 print ttitle.encode('utf_8')
-                print t
 
 if __name__ == '__main__':
     # Get command line options
@@ -62,14 +62,17 @@ if __name__ == '__main__':
 
        If arg1 arg2... are given, only tasks which include
        these words (ORed) will be listed (combined with -i)
+
+       If you have not installed google-api-python-client, follow:
+       https://developers.google.com/api-client-library/python/start/installation
        '''
     parser = OptionParser(usage)
-    parser.add_option('-l','--list',action='store',
-                        dest='uselist',default='',
-                        help='If \'USELIST\' is not an empty, tasks will be searched for only from the given list. [default: %default]')
-    parser.add_option('-i','--item',action='store',
-                        dest='item',default='',
-                        help='If \'ITEM\' is not an empty, only tasks which contains given item are listed up. Multiple words can be given by using \',\' as a separator. [default: %default]')
+    parser.add_option('-l', '--list', action='store',
+                      dest='uselist', default='',
+                      help='If \'USELIST\' is not an empty, tasks will be searched for only from the given list. [default: %default]')
+    parser.add_option('-i', '--item', action='store',
+                      dest='item', default='',
+                      help='If \'ITEM\' is not an empty, only tasks which contains given item are listed up. Multiple words can be given by using \',\' as a separator. [default: %default]')
     (opts, args) = parser.parse_args()
 
     # Set parameters
@@ -79,5 +82,4 @@ if __name__ == '__main__':
         items.append(i)
 
     # Run main function
-    main(uselist,items)
-
+    main(uselist, items)
