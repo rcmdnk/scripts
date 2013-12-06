da6610663c7186ba06b69327f4eab9448ac2eb8a
rcmdnk <rcmdnk@gmail.com>
2013-12-06 03:25:24 +0100

    install.sh, from darwin13

--- install.sh
+++ install.sh
@@ -66,7 +66,7 @@ if [[ "$OSTYPE" =~ "cygwin" ]];then
     fi
     t_winpath=$(cygpath -w -a "$target")
     t_link=$(cygpath -w -a "$link")
-    cmd /c mklink $opt "$t_link" "$t_winpath"
+    cmd /c mklink $opt "$t_link" "$t_winpath" > /dev/null
   }
 # }}}
 fi
