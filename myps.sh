a90a7e70a57def4b2abda12d16e66e5500770f25
rcmdnk <rcmdnk@gmail.com>
2013-12-08 03:26:38 +0100

    bkup.sh    myps.sh    submodules/trash (new commits), from darwin13

--- myps.sh
+++ myps.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 NPROCESSES=10
-ps axr -o %cpu -o %mem -o comm|head -n$((NPROCESSES+1))|tail -n$NPROCESSES >/tmp/pstmp
 echo "  CPU  MEM  CMD"
-while read line;do
+ps axr -o %cpu -o %mem -o comm|head -n$((NPROCESSES+1))|tail -n$NPROCESSES| while read line;do
   cpu=`echo ${line[*]}|cut -d' ' -f1`
   mem=`echo ${line[*]}|cut -d' ' -f2`
   cmd=`echo ${line[*]}|cut -d' ' -f3-`
@@ -10,5 +9,4 @@ while read line;do
     cmd=`echo ${cmd[*]} |awk -F, '{n=split($1,tmp,"/")}{print tmp[n]}'`
   fi
   printf "%5s%5s %s\n" $cpu $mem $cmd
-done < /tmp/pstmp
-rm -r /tmp/pstmp
+done
