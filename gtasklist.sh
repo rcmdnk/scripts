ba6cdd1913e72f7b23490cfdb27c27c13f6e9b02
rcmdnk <rcmdnk@gmail.com>
2014-02-17 03:26:28 +0100

    gtasklist.sh    submodules/apt-cyg (new commits)    submodules/evernote_mail (new commits)    submodules/multi_clipboard (new commits)    submodules/sd_cl (new commits)    submodules/sentaku (new commits)    submodules/trash (new commits), from darwin13.0.0

--- gtasklist.sh
+++ gtasklist.sh
@@ -7,7 +7,8 @@ fgtasklist=~/.gtasklist
 # get tasks
 tmpfile=$(mktemp 2>/dev/null||mktemp -t tmp)
 gtask -l 00_Inbox -i IMPORTANT > "$tmpfile"
-if [ $(cat "$tmpfile"|wc -l) -gt 0 ];then
+ret=$?
+if [ $ret -eq 0 ];then
   cp "$tmpfile" "$fgtasklist"
 fi
 rm -f "$tmpfile"
