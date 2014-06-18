241b9fc7c4eb3bc99d3defd66f2e833eac2232d9
rcmdnk <rcmdnk@gmail.com>
2014-06-18 17:13:30 +0900

    added updateGIT.sh

---
+++ updateGIT.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+. ~/.bashrc
+cd $(dirname $0)
+
+for dir in dotfiles scripts;do
+  if [ -d $dir ];then
+    cd $dir
+    cd submodules
+    git submodule foreach git update
+    git update
+    cd ../
+  fi
+done
