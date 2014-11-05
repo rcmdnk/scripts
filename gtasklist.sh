7fe46d8fc76b36270380ea5a87ddedc7ed94a06b
rcmdnk <rcmdnk@gmail.com>
2014-11-06 05:03:32 +0900

    (commit or discard the untracked or modified content in submodules)    gtasklist.sh    submodules/sd_cl (untracked content), from darwin14

--- gtasklist.sh
+++ gtasklist.sh
@@ -6,7 +6,7 @@ fgtasklist=~/.gtasklist
 
 # get tasks
 tmpfile=$(mktemp 2>/dev/null||mktemp -t tmp)
-gtask -l 00_Inbox -i IMPORTANT > "$tmpfile"
+gtask -l 00_Inbox -i IMPORTANT |sed 's/IMPORTANT//g' > "$tmpfile"
 ret=$?
 if [ $ret -eq 0 ];then
   cp "$tmpfile" "$fgtasklist"
