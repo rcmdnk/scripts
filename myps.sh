c71d94463e0fac6a507624ef8f6b54690ee09f36
rcmdnk <rcmdnk@gmail.com>
2014-12-02 22:17:09 +0900

    refactored with shellcheck

--- myps.sh
+++ myps.sh
@@ -2,9 +2,9 @@
 NPROCESSES=10
 echo "  CPU  MEM  CMD"
 ps axr -o %cpu -o %mem -o comm|head -n$((NPROCESSES+1))|tail -n$NPROCESSES| while read line;do
-  cpu=$(echo "${line[*]}"|cut -d' ' -f1)
-  mem=$(echo "${line[*]}"|cut -d' ' -f2)
-  cmd=$(echo "${line[*]}"|cut -d' ' -f3-)
+  cpu=$(echo "${line[*]}"|awk '{print $1}')
+  mem=$(echo "${line[*]}"|awk '{print $2}')
+  cmd=$(echo "${line[*]}"|awk '{for(i=3;i<NF;i++){printf("%s ",$i,OFS=" ")}print $NF}')
   if echo "${cmd[*]}"|grep -q "/";then
     cmd=$(echo "${cmd[*]}" |awk -F, '{n=split($1,tmp,"/")}{print tmp[n]}')
   fi
