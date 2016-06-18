ac6f0f8ebe14edfefe52c7bec5d37076463ef8f9
rcmdnk <rcmdnk@gmail.com>
2016-06-19 07:16:11 +0900

    updateGIT.sh, from linux-gnu

--- updateGIT.sh
+++ updateGIT.sh
@@ -5,7 +5,15 @@ cd "$(dirname "$0")"
 for dir in dotfiles scripts;do
   if [ -d $dir ];then
     cd $dir
-    git submodule -q foreach --recursive git update
+    #git submodule -q foreach --recursive git update
+    for d in external/*;do
+      cd $d
+      git pull
+    done
+    for d in submodules/*;do
+      cd $d
+      git update
+    done
     git update
     ./install.sh -b "" >& /dev/null
     cd ../
