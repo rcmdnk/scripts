c46ae782326a3272be2763238cb0aa8fec323ea3
rcmdnk <rcmdnk@gmail.com>
2016-06-28 16:36:38 +0900

    external/gcalcli (new commits)    updateGIT.sh, from linux-gnu

--- updateGIT.sh
+++ updateGIT.sh
@@ -6,14 +6,20 @@ for dir in dotfiles scripts;do
   if [ -d $dir ];then
     cd $dir
     #git submodule -q foreach --recursive git update
-    for d in external/*;do
-      cd $d
-      git pull
-    done
-    for d in submodules/*;do
-      cd $d
-      git update
-    done
+    if [ -d external ];then
+      for d in external/*;do
+        cd $d
+        git pull
+        cd -
+      done
+    fi
+    if [ -d submodules ];then
+      for d in submodules/*;do
+        cd $d
+        git update
+        cd -
+      done
+    fi
     git update
     ./install.sh -b "" >& /dev/null
     cd ../
