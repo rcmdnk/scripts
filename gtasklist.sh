cc73221957c3b3bad2c9e4072d774aebe8704beb
rcmdnk <rcmdnk@gmail.com>
2018-03-01 03:02:47 +0900

    (commit or discard the untracked or modified content in submodules)    gtasklist.sh    submodules/gtask (modified content), from darwin17

--- gtasklist.sh
+++ gtasklist.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+. ~/.bashrc
+
 # File
 fgtasklist=~/.gtasklist
 
 # get tasks
 tmpfile=$(mktemp 2>/dev/null||mktemp -t tmp)
-gtask -l 00_Inbox -i IMPORTANT |sed 's/IMPORTANT//g' > "$tmpfile"
+gtask -l 00_Inbox > "$tmpfile"
 ret=$?
 if [ $ret -eq 0 ];then
   cp "$tmpfile" "$fgtasklist"
